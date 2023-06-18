bee23f28c779cfbfa0d049a16c60d3e16a1ad57b
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-06-18 19:42:01 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -5,35 +5,36 @@
 
 using namespace std;
 #define KMAX 10000
-long long K, N, l, m, h;
-vector<int> lens(KMAX);
+int K, N, l, m, h;
+vector<int> lengths(KMAX);
 
 bool check(int x) {
     long long cnt = 0;
-    // lens[i]/x <= 2^31-1(INT_MAX) 이므로 여러개 더해지면 cnt 범위 int 초과
     for (int i = 0; i < K; i++) {
-        cnt += lens[i] / x;
+        cnt += lengths[i] / x;
     }
     return cnt >= N;
 }
 
 void sol() {
     cin >> K >> N;
-    // 1<=len<=2^31-1
+    // 1<=length<=2^31-1
     l = 1;
-    h = pow(2, 31);
+    h = pow(2, 31) - 1;
     for (int i = 0; i < K; i++) {
-        cin >> lens[i];
-        //h = min(h, lens[i]); // 범위 안좁혀도 큰 상관x
+        cin >> lengths[i];
+        //h = min(h, lengths[i]); // 범위 안좁혀도 큰 상관x
     }
-
-    while (l + 1 < h) {
-        m = ((long long)l + h) / 2; // h=2^31-1(INT_MAX) 면 l+h가 int 범위 초과
-        // cout << "l: " << l << "m: " << m << "h: " << h << endl;
-        if (check(l) == check(m)) l = m;
-        if (check(h) == check(m)) h = m;
+    if (check(h)) cout << h;
+    else {
+        while (l + 1 < h) {
+            m = ((long long)l + h) / 2; //h=2^31-1(INT_MAX) 면 l+h가 int 범위 초과
+            //cout << "l: " << l << "m: " << m << "h: " << h << endl;
+            if (check(l) == check(m)) l = m;
+            if (check(h) == check(m)) h = m;
+        }
+        cout << l;
     }
-    cout << l;
 }
 
 int32_t main(void)
