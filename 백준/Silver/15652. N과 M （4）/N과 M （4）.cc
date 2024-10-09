db7b51691de1568de3b09b176f38f63f8d33c497
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-10-09 23:19:18 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+#define endl '\n';
+int M, N;
+int arr[9];
+
+template <typename T>
+void printArr(T arr) {
+	for (int i = 0; i < M; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void backtracking(int confirmedNum) {
+	if (confirmedNum == M) {
+		printArr<int*>(arr);
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (confirmedNum > 0 && arr[confirmedNum - 1] > i) {
+			continue;
+		}
+		arr[confirmedNum] = i;
+		backtracking(confirmedNum + 1);
+	}
+}
+
+void backtrackingWithStack() {
+	stack<vector<int>> s;
+	s.push(vector<int> {});
+	while (!s.empty()) {
+		vector<int> v = s.top();
+		s.pop();
+		if (v.size() == M) {
+			printArr<vector<int>>(v);
+			continue;
+		}
+		for (int i = N; i >= 1; i--) {
+			if (!v.empty() && v.back() > i) {
+				continue;
+			}
+			v.push_back(i);
+			s.push(vector<int>(v));
+			v.pop_back();
+		}
+	}
+}
+
+int32_t main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M;
+	backtracking(0);
+	//backtrackingWithStack();
+}
