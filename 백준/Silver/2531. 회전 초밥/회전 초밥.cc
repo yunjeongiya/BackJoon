c6e17ae6aa5677e5cf239640c7cdc0e2deb974d6
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-11-20 00:39:33 +0900

    [Silver I] Title: 회전 초밥, Time: 352 ms, Memory: 13284 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cc
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -9,9 +9,11 @@ int main(void) {
 	cin >> N >> d >> k >> c;
 	vector<vector<bool>> masking(d + 1, vector<bool>(N, false));
 	vector<int> cnt(N, 0);
+	int max = 0;
 	for (int i = 0; i < N; i++) {
 		if (!masking[c][i]) {
 			cnt[i]++;
+			if (max < cnt[i]) max = cnt[i];
 		}
 		masking[c][i] = true;
 		int sushi;
@@ -19,21 +21,10 @@ int main(void) {
 		for (int j = 0; j < k; j++) {
 			if (!masking[sushi][(i + j) % N]) {
 				cnt[(i + j) % N]++;
+				if (max < cnt[(i + j) % N]) max = cnt[(i + j) % N];
 			}
 			masking[sushi][(i + j) % N] = true;
 		}
-	}
-	//for (int i = 0; i < d; i++) {
-	//	for (int j = 0; j < N; j++) {
-	//		if (masking[i][j]) cout << 'o';
-	//		else cout << 'x';
-	//		cout << ' ';
-	//	}
-	//	cout << endl;
-	//}
-	int max = 0;
-	for (int i = 0; i < N; i++) {
-		if (max < cnt[i]) max = cnt[i];
 	}
 	cout << max;
 }
