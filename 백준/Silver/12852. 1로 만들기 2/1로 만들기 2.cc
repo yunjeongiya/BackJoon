2acc4fd4ac49e8d166e3f31d78e71d86f9296a18
윤정 <yunjeongiya@gmail.com>
2024-12-21 14:35:07 +0900

    [Silver I] Title: 1로 만들기 2, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -3,8 +3,7 @@ using namespace std;
 #define endl '\n'
 #define MAX 1000001
 
-int dist[MAX] = { 0, 0, };
-int pre[MAX] = { -1, -1, };
+int dist[MAX] = { -1, 0, };
 int main(void) {
 	cin.tie(0);
 	cout.tie(0);
@@ -13,13 +12,15 @@ int main(void) {
 	cin >> N;
 	for (int i = 2; i <= N; i++) {
 		dist[i] = dist[i - 1] + 1;
-		pre[i] = i - 1;
-		if (i % 2 == 0 && dist[i] > dist[i / 2] + 1) { dist[i] = dist[i / 2] + 1; pre[i] = i / 2; }
-		if (i % 3 == 0 && dist[i] > dist[i / 3] + 1) { dist[i] = dist[i / 3] + 1; pre[i] = i / 3; }
+		if (i % 2 == 0 && dist[i] > dist[i / 2] + 1) { dist[i] = dist[i / 2] + 1; }
+		if (i % 3 == 0 && dist[i] > dist[i / 3] + 1) { dist[i] = dist[i / 3] + 1; }
 	}
 	cout << dist[N] << endl;
-	while (N != -1) {
-		cout << N << ' ';
-		N = pre[N];
+	int i = N;
+	while (i != 0) {
+		cout << i << ' ';
+		if (dist[i] == dist[i - 1] + 1) i = i - 1;
+		else if (i % 2 == 0 && dist[i] == dist[i / 2] + 1) { i = i / 2; }
+		else if (i % 3 == 0 && dist[i] == dist[i / 3] + 1) { i = i / 3; }
 	}
 }
