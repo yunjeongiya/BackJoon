bb4bccd2f66fab5a4c07c7e48382cf84d5db79a2
윤정 <yunjeongiya@gmail.com>
2025-01-01 10:49:26 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 244 ms, Memory: 5732 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <map>
 using namespace std;
 #define endl '\n';
 
-int N, E;
-vector<vector<pair<int, int>>> G;
-
-int dijkstra(int from, int to) {
-	vector<int> dist(N + 1, -1);
+void dijkstra(int from, vector<int>& dist, vector<vector<pair<int, int>>>& G) {
 	dist[from] = 0;
 	priority_queue<pair<int, int>> PQ;
 	PQ.push({ 0, from });
@@ -24,13 +21,12 @@ int dijkstra(int from, int to) {
 			PQ.push({ newCost, e.first });
 		}
 	}
-	return dist[to];
 }
 
-int getDistOfPath(vector<pair<int, int>> path) {
+int getDistOfPath(vector<pair<int, int>> path, map<int, vector<int>>& distVecMap) {
 	int sum = 0;
 	for (auto& part : path) {
-		int partDist = dijkstra(part.first, part.second);
+		int partDist = distVecMap[part.first][part.second];
 		if (partDist == -1) {
 			return -1;
 		}
@@ -44,6 +40,8 @@ int main(void){
 	cout.tie(0);
 	ios::sync_with_stdio(0);
 
+	int N, E;
+	vector<vector<pair<int, int>>> G;
 	cin >> N >> E;
 	G = vector<vector<pair<int, int>>>(N + 1);
 
@@ -55,9 +53,18 @@ int main(void){
 	}
 	int v1, v2;
 	cin >> v1 >> v2;
+	map<int, vector<int>> distVecMap({
+		{1, vector<int>(N + 1, -1)},
+		{v1, vector<int>(N + 1, -1)},
+		{v2, vector<int>(N + 1, -1)},
+		});
 
-	int sol1 = getDistOfPath({ {1, v1}, {v1, v2}, {v2, N} });
-	int sol2 = getDistOfPath({ {1, v2}, {v2, v1}, {v1, N} });
+	dijkstra(1, distVecMap[1], G);
+	dijkstra(v1, distVecMap[v1], G);
+	dijkstra(v2, distVecMap[v2], G);
+
+	int sol1 = getDistOfPath({ {1, v1}, {v1, v2}, {v2, N} }, distVecMap);
+	int sol2 = getDistOfPath({ {1, v2}, {v2, v1}, {v1, N} }, distVecMap);
 	if (sol1 == -1 && sol2 == -1) cout << -1;
 	else if (sol1 == -1 || sol2 < sol1) cout << sol2;
 	else cout << sol1;
