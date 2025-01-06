04a021052d7f41af97b7e9c5722d328d40a8b1e7
윤정 <yunjeongiya@gmail.com>
2025-01-07 00:50:47 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2568 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.cc
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 #define endl '\n'
 #define MAX 10000
@@ -24,23 +25,20 @@ int main(void) {
 		if (i != MAX) roadsFrom[i].push_back({ i + 1, 1 });
 	}
 
-	cnt = MAX;
-	while (cnt--) {
-		int curMin = -1;
-		int curMinDist = MAX;
-		for (int i = 0; i <= MAX; i++) {
-			if (!confirmed[i] && curMinDist > dist[i]) {
-				curMinDist = dist[i];
-				curMin = i;
-			}
-		}
-		if (curMin == -1) break;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+	pq.push({ 0, 0 });
+	while (!pq.empty()) {
+		int curMinDist = pq.top().first;
+		int curMin = pq.top().second;
+		pq.pop();
+		if (confirmed[curMin]) continue;
 		confirmed[curMin] = true;
 
-		for (auto& roads : roadsFrom[curMin]) {
-			if (confirmed[roads.first]) continue;
-			if (dist[roads.first] < curMinDist + roads.second) continue;
-			dist[roads.first] = curMinDist + roads.second;
+		for (auto& road : roadsFrom[curMin]) {
+			if (confirmed[road.first]) continue;
+			if (dist[road.first] < curMinDist + road.second) continue;
+			dist[road.first] = curMinDist + road.second;
+			pq.push({ dist[road.first], road.first });
 		}
 	}
 
