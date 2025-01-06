8742b6010cfa29bc76da3fc2326650a7b50576bf
윤정 <yunjeongiya@gmail.com>
2025-01-06 23:07:19 +0900

    [Silver I] Title: 지름길, Time: 132 ms, Memory: 2568 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define endl '\n'
+#define MAX 10000
+
+int dist[MAX + 1];
+bool confirmed[MAX + 1] = { 0, };
+int main(void) {
+	int N, D;
+	cin >> N >> D;
+	int cnt = N;
+	vector<vector<pair<int, int>>> roadsFrom(MAX + 1);
+
+	while (cnt--) {
+		int start, end, dist;
+		cin >> start >> end >> dist;
+		roadsFrom[start].push_back({ end, dist });
+	}
+
+	for (int i = 0; i < MAX; i++) {
+		dist[i] = i;
+		roadsFrom[i].push_back({ i + 1, 1 });
+	}
+	dist[MAX] = MAX;
+
+	cnt = MAX;
+	while (cnt--) {
+		int curMin = -1;
+		int curMinDist = MAX;
+		for (int i = 0; i <= MAX; i++) {
+			if (!confirmed[i] && curMinDist > dist[i]) {
+				curMinDist = dist[i];
+				curMin = i;
+			}
+		}
+		if (curMin == -1) break;
+		confirmed[curMin] = true;
+
+		for (auto& roads : roadsFrom[curMin]) {
+			if (confirmed[roads.first]) continue;
+			if (dist[roads.first] < curMinDist + roads.second) continue;
+			dist[roads.first] = curMinDist + roads.second;
+		}
+	}
+
+	cout << dist[D];
+}
