44929566fa3cd9a7708c1857dff75cd7b6426379
윤정 <yunjeongiya@gmail.com>
2025-01-10 01:14:36 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 420 ms, Memory: 15384 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -25,14 +25,13 @@ void go() {
 			int ny = dy[i] + y;
 			int nx = dx[i] + x;
 			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if (visited[ny][nx] && macnt[ny][nx] <= cnt) continue; // 더 적게 부수고 갈 수 있을 때
 			if (cnt < k && a[ny][nx]) { //부술 기회 남았고 벽일 때
-				if (visited[ny][nx] && macnt[ny][nx] <= cnt) continue;
 					visited[ny][nx] = visited[y][x] + 1;
 					macnt[ny][nx] = cnt + 1;
 					q.push({ ny,nx, cnt + 1 });
 			}
 			if (a[ny][nx]) continue; // 부술 기회 없는데 벽일 때
-			if (visited[ny][nx] && macnt[ny][nx] <= cnt) continue; // 더 적게 부수고 갈 수 있을 때
 			visited[ny][nx] = visited[y][x] + 1; 
 			macnt[ny][nx] = cnt;
 			q.push({ ny,nx, cnt });
