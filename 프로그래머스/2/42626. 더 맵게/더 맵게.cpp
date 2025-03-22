a0bfe69e2596d62258dc69c23cee3303861dd6ce
윤정 <yunjeongiya@gmail.com>
2025-03-22 16:25:24 +0900

    [level 2] Title: 더 맵게, Time: 250.17 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int> q;
+    for(int s : scoville) {
+        q.push(-s);
+    }
+    while(true) {
+        if(-q.top() >= K) break;
+        if(q.size() < 2) return -1;
+        int news = q.top();
+        q.pop();
+        news += 2*q.top();
+        q.pop();
+        q.push(news);
+        answer++;
+    }
+    return answer;
+}
