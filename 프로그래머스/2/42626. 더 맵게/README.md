b8fbf64c5a29d1fbeb3ed86d082890e790d93297
윤정 <yunjeongiya@gmail.com>
2025-03-22 16:59:27 +0900

    [level 2] Title: 더 맵게, Time: 214.05 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 39.7 MB, 시간: 250.17 ms
+메모리: 41.2 MB, 시간: 214.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 22일 16:25:22
+2025년 03월 22일 16:59:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    priority_queue<int> q;
+    priority_queue<int, vector<int>, greater<int>> q;
     for(int s : scoville) {
-        q.push(-s);
+        q.push(s);
     }
     while(true) {
-        if(-q.top() >= K) break;
+        if(q.top() >= K) break;
         if(q.size() < 2) return -1;
         int news = q.top();
         q.pop();
