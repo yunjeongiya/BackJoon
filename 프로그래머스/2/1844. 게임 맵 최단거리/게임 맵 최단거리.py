27fd9e4da2de2c59aead531ce1529a765f40975f
윤정 <yunjeongiya@gmail.com>
2025-04-16 03:46:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.34 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,27 @@
 from collections import deque
 
+dx = (0, 0, 1, -1)
+dy = (1, -1, 0, 0)
 def solution(maps):
-    # 맵 크기: n은 행(row)의 개수, m은 열(column)의 개수
-    n, m = len(maps), len(maps[0])
-    
-    # 이동 방향 (상, 하, 좌, 우)
-    # dx: 행 방향, dy: 열 방향
-    dx = [-1, 1, 0, 0]  # 위로(-1), 아래로(+1), 그대로(0), 그대로(0)
-    dy = [0, 0, -1, 1]  # 그대로(0), 그대로(0), 왼쪽(-1), 오른쪽(+1)
-    
-    # BFS 함수
-    def bfs(x, y):
-        queue = deque([(x, y)])  # 시작 좌표를 큐에 추가
-        
-        while queue:
-            x, y = queue.popleft()  # 현재 좌표
+    #가중치 없는 그래프 -> BFS
+    n = len(maps)
+    m = len(maps[0])
+    q = deque()
+    q.append((0, 0))
+    while len(q) != 0:
+        x, y = q.popleft()
+        for i in range(4):
+            nextX = x + dx[i]   
+            nextY = y + dy[i]
             
-            # 현재 위치에서 4방향 탐색
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]  # 새로운 좌표 (행: nx, 열: ny)
+            if (nextX == 0 and nextY == 0) or (nextX < 0 or nextX >= n or nextY < 0 or nextY >= m):
+                continue
+            if (maps[nextX][nextY] != 1):
+                continue
                 
-                # 맵 범위 안에 있고, 벽이 아니며 아직 방문하지 않은 경우
-                if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
-                    # 이동 거리 기록 (이전 거리 + 1)
-                    maps[nx][ny] = maps[x][y] + 1
-                    queue.append((nx, ny))
-        
-        # 상대 팀 진영 (행: n-1, 열: m-1)의 값이 초기 값(1)에서 변경되지 않았다면 도달 불가
-        return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1
-
-    # BFS 탐색 시작
-    return bfs(0, 0)  # 좌측 상단 (행: 0, 열: 0)에서 시작
+            maps[nextX][nextY] += maps[x][y]
+            if nextX == n-1 and nextY == m-1:
+                return maps[nextX][nextY]
+            q.append((nextX, nextY))
+            
+    return -1
