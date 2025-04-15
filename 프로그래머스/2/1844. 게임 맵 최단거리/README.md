1fa7120cffe0ba3741fb293bffe505339e6ee308
윤정 <yunjeongiya@gmail.com>
2025-04-16 03:49:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.38 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.1 MB, 시간: 0.17 ms
+메모리: 9.46 MB, 시간: 12.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 16일 03:46:53
+2025년 04월 16일 03:49:28
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,27 +1,19 @@
 from collections import deque
 
-dx = (0, 0, 1, -1)
-dy = (1, -1, 0, 0)
+dx, dy = (0, 0, 1, -1), (1, -1, 0, 0)
 def solution(maps):
     #가중치 없는 그래프 -> BFS
-    n = len(maps)
-    m = len(maps[0])
+    n, m = len(maps), len(maps[0])
     q = deque()
     q.append((0, 0))
     while len(q) != 0:
         x, y = q.popleft()
         for i in range(4):
-            nextX = x + dx[i]   
-            nextY = y + dy[i]
-            
-            if (nextX == 0 and nextY == 0) or (nextX < 0 or nextX >= n or nextY < 0 or nextY >= m):
-                continue
-            if (maps[nextX][nextY] != 1):
-                continue
-                
-            maps[nextX][nextY] += maps[x][y]
-            if nextX == n-1 and nextY == m-1:
-                return maps[nextX][nextY]
-            q.append((nextX, nextY))
+            nextX, nextY = x + dx[i], y + dy[i]
+            if 0 <= nextX < n and 0 <= nextY < m and maps[nextX][nextY] == 1:                
+                maps[nextX][nextY] += maps[x][y]
+                if nextX == n-1 and nextY == m-1:
+                    return maps[nextX][nextY]
+                q.append((nextX, nextY))
             
     return -1
